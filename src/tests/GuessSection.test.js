018382f1781c9a6c93052c2608060639b546b6a4
deconstructionalism <deconstructionalism@gmail.com>
2018-03-24 15:06:21 -0400

    added submit test

--- src/tests/GuessSection.test.js
+++ src/tests/GuessSection.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import GuessSection from '../components/GuessSection';
 
@@ -7,4 +7,18 @@ describe('<GuessSection />', () => {
     it('Render without crashing', () => {
         shallow(<GuessSection />);
     });
+    it('Should fire the onMakeGuess callback when the GuessForm is submitted', () => {
+        const callback = jest.fn();
+        const wrapper = mount(<GuessSection onMakeGuess={callback} />);
+        // the reason value is a string is because we are setting the value
+        // of an input box which is always a string
+        const value = "5";
+        wrapper.find('#userGuess').instance().value = value;
+        wrapper.find('GuessForm form').simulate('submit');
+        expect(callback).toHaveBeenCalledWith(value);
+      })
 });
+
+
+
+  
