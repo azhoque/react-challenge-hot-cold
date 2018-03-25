4169bf43fb9029ddb75282043bd3a9ddf8d12f45
Azizi <azhoque@gmail.com>
2018-03-25 10:39:58 -0500

    app test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import App from '../pages/App';
 
 describe('<App />', () => {
   it('Render without crashing', () => {
       shallow(<App />);
+  });
+  it('Changes state.guesses in app when makeGuess is called', () => {
+    //const callback = jest.fn();
+    const wrapper = mount(<App />);// 
+    wrapper.setState ({correctAnswer : 50}); 
+    wrapper.instance().makeGuess(50);
+    expect(wrapper.state('guesses')).toEqual([50]);
+    expect(wrapper.state('feedback')).toEqual('You got it!');
+   // expect(callback).toHaveBeenCalledWith(correctAnswer);
   });
 });
